94e443f134bc8d6576aa3979a9e714c70c081f06
Leon.van.der.Ree <Leon.van.der.Ree@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-05-17 18:03:51 -0500

    small mods to sfExtjs3Object
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtjs3Plugin/trunk@18395 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/model/sfExtjs3Object.class.php
+++ lib/model/sfExtjs3Object.class.php
@@ -7,164 +7,182 @@
  */
 class sfExtjs3Object
 {
-	const FUNCTION_SEPARATOR = ' : ';
+  const FUNCTION_SEPARATOR = ' : ';
 
-	protected
-	 $objectName,
-	 $baseObject,
-	 $functions = array();
+  protected
+   $objectName,
+   $baseObject,
+   $functions = array();
 
-	/**
-	 * Creates a new JsObject
-	 *
-	 * @param string $objectName the name of the new class to be created (can be preceded with dotted namespace
-	 * @param string $baseObject the base object which this new class should extend
-	 * @param array $functions   an associative array of function-names and the sfExtjs3Function itself
-	 */
-	public function __construct($objectName, $baseObject, $functions = array())
-	{
-		$this->objectName = trim($objectName);
-		$this->baseObject = trim($baseObject);
+  /**
+   * Creates a new JsObject
+   *
+   * @param string $objectName the name of the new class to be created (can be preceded with dotted namespace
+   * @param string $baseObject the base object which this new class should extend
+   * @param array $functions   an associative array of function-names and the sfExtjs3Function itself
+   */
+  public function __construct($objectName, $baseObject, $functions = array())
+  {
+    $this->objectName = trim($objectName);
+    $this->baseObject = trim($baseObject);
 
-		if (!is_array($functions))
-		{
-			throw new Exception('argument "functions" should be an array');
-		}
-		foreach ($functions as $functionName => $function)
-		{
-			$this->addFunction($functionName, $function);
-		}
-		
-		$this->configure();
-	}
-	
-	/**
-	 * The configure function can be defined in extensions of this class
-	 *
-	 */
-	public function configure()
-	{
-		
-	}
+    if (!is_array($functions))
+    {
+      throw new Exception('argument "functions" should be an array');
+    }
+    foreach ($functions as $functionName => $function)
+    {
+      $this->addFunction($functionName, $function);
+    }
 
-	public function getFunctions()
-	{
-		return $this->functions;
-	}
+    $this->configure();
+  }
 
-	/**
-	 * Adds a function to the extjs-object
-	 *
-	 * @param string $functionName
-	 * @param sfExtjs3Function $function
-	 */
-	public function addFunction($functionName, sfExtjs3Function $function)
-	{
-		if (isset($this->functions[$functionName]))
-		{
-			throw new Exception('The function "'.$functionName.'" is already defined for object "'.$this->objectName.'".');
-		}
+  /**
+   * The configure function can be defined in extensions of this class
+   *
+   */
+  public function configure()
+  {
+  }
 
-		$this->functions[$functionName] = $function;
-	}
+  /**
+   * Gets all defined functions
+   *
+   * @return array an associative array with all defined functions
+   */
+  public function getFunctions()
+  {
+    return $this->functions;
+  }
 
-	/**
-	 * returns the name of this object
-	 *
-	 */
-	public function getName()
-	{
-		return $this->objectName;
-	}
+  /**
+   * Adds a function to the extjs-object
+   *
+   * @param string $functionName
+   * @param sfExtjs3Function $function
+   */
+  public function addFunction($functionName, sfExtjs3Function $function)
+  {
+    if (isset($this->functions[$functionName]))
+    {
+      throw new Exception('The function "'.$functionName.'" is already defined for object "'.$this->getName().'".');
+    }
 
-	/**
-	 * renders the construction for a new instance for this object
-	 *
-	 * @param  string, the variable names that should be provided to the constructor
-	 * @return string
-	 */
-	public function renderConstruction()
-	{
-		$args = implode(', ', func_get_args());
+    $this->functions[$functionName] = $function;
+  }
 
-		$js  = 'new '.$this->getName().'(';
-	  $js .= $args;
-		$js .= ')';
+  /**
+   * returns the name of this object
+   *
+   */
+  public function getName()
+  {
+    return $this->objectName;
+  }
 
-		return $js;
-	}
+  /**
+   * renders the construction for a new instance for this object
+   *
+   * @param  array $args the variable names that should be provided to the constructor
+   * @return string
+   */
+  public function renderConstruction($args = array())
+  {
+    $args = implode(', ', $args);
 
-	/**
-	 * Renders the calling of a function for this object
-	 *
-	 * @param string $functionName the function name (can be dotted for example to call superclass functions)
-	 * @param array $arguments     the arguments that are called for this function
-	 * @return string              the function call
-	 */
-	public function renderFunctionCall($functionName, $arguments = array())
-	{
+    $js  = 'new '.$this->getName().'(';
+    $js .= $args;
+    $js .= ')';
+
+    return $js;
+  }
+
+  /**
+   * Renders the registering of the xtype of this class, can
+   * be called after rendering the definition of this class
+   *
+   * @param string  $xtype  The name of the xtype to register this class as
+   * @return string
+   */
+  public function registerXtypeAs($xtype)
+  {
+    $js = "Ext.reg('".$xtype."', ".$this->getName().");\n";
+
+    return $js;
+  }
+
+  /**
+   * Renders the calling of a function for this object
+   *
+   * @param string $functionName the function name (can be dotted for example to call superclass functions)
+   * @param array $arguments     the arguments that are called for this function
+   * @return string              the function call
+   */
+  public function renderFunctionCall($functionName, $arguments = array())
+  {
     if (!is_array($arguments))
     {
       throw new Exception('arguments should be an array');
     }
 
-		$js  = $this->getName().'.'.$functionName.'(';
-		$js .= implode(', ', $arguments);
-		$js .= ')';
+    $js  = $this->getName().'.'.$functionName.'(';
+    $js .= implode(', ', $arguments);
+    $js .= ')';
 
-		return $js;
-	}
+    return $js;
+  }
 
-	/**
-	 * text representation of all functions defined for this object
-	 *
-	 * @return string a concatenation of of all functions surrounded by {}
-	 */
-	public function renderFunctionDefinitions()
-	{
-		$js  = '{';
+  /**
+   * text representation of all functions defined for this object
+   *
+   * @return string a concatenation of of all functions surrounded by {}
+   */
+  public function renderFunctionDefinitions()
+  {
+    $js  = '{';
 
-		$functions = $this->getFunctions();
-		if (count($functions))
-		{
-			list($firstFunctionName, $firstFunction) = each($functions);
+    $functions = $this->getFunctions();
+    if (count($functions))
+    {
+      list($firstFunctionName, $firstFunction) = each($functions);
 
-			$js .= "\n".$firstFunctionName.self::FUNCTION_SEPARATOR.$firstFunction;
+      $js .= "\n".$firstFunctionName.self::FUNCTION_SEPARATOR.$firstFunction;
 
-			while (list($functionName, $function) = each($functions))
-			{
-				$js .= ",\n".$functionName.self::FUNCTION_SEPARATOR.$function;
-			}
+      while (list($functionName, $function) = each($functions))
+      {
+        $js .= ",\n".$functionName.self::FUNCTION_SEPARATOR.$function;
+      }
 
-			$js .= "\n";
-		}
+      $js .= "\n";
+    }
 
-		$js .= '}';
+    $js .= '}';
 
-		return $js;
-	}
+    return $js;
+  }
 
-	/**
-	 * renders the object definition
-	 *
-	 * @return string
-	 */
-	public function renderDefinition()
-	{
-		$js = $this->getName();
+  /**
+   * renders the object definition
+   *
+   * @return string
+   */
+  public function renderDefinition()
+  {
+    $js = $this->getName();
 
-		$js .= ' = Ext.extend('.$this->baseObject.', ';
+    $js .= ' = Ext.extend('.$this->baseObject.', ';
 
-		$js .= $this->renderFunctionDefinitions();
+    $js .= $this->renderFunctionDefinitions();
 
-		$js .= ');';
+    $js .= ");\n";
 
-		return $js;
-	}
+    return $js;
+  }
 
-	/**
-	 * @see renderDefinition()
-	 */
+  /**
+   * @see renderDefinition()
+   */
   public function __toString()
   {
     return $this->renderDefinition();
